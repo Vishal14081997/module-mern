ea54a8c754ecd823c89bd5341600e2d41b3563c2
Vishal14081997 <1408vishalsingh@gmail.com>
2026-06-19 15:38:35 +0530

    event , os , http sever create

--- modules/file.js
+++ modules/file.js
@@ -1,7 +1,8 @@
 const fs = require("fs")
+const os = require("os")
 
 // Sync....
-// fs.writeFileSync("./test.txt" ,"hello world");
+// fs.writeFileSync("./test.txt", "hello world");
 
 // Async...
 // fs.writeFile("./test.txt","hello world async" ,(err)=>{})
@@ -14,7 +15,7 @@ const fs = require("fs")
 //         console.log("error" ,err);
 //     }else{
 //         console.log(result);
-    
+
 //     }
 // })
 
@@ -28,6 +29,105 @@ const fs = require("fs")
 // fs.unlinkSync("./copy.txt") - delete file 
 
 // fs.mkdirSync("new_docs/a/b" ,{recursive:true})
+
+// fs.mkdirSync("page")
+// fs.rmdirSync("page")
+
+
+
+// os  -> apki machin jis pr bhi kam krti h us ki information nikalne m kam aata h
+
+// console.log(os.platform()); // -> windows ka version
+// console.log(os.arch());
+// console.log(os.hostname());
+// console.log(os.homedir());
+// console.log(os.freemem());
+
+const EventEmitter = require("events")  // constructor
+const event = new EventEmitter();
+
+// event.on("data" ,()=>{
+//     console.log("this is data event"); 
+// })
+// event.on("data" ,()=>{
+//     console.log("this is data 2nd event"); 
+// })
+// event.emit("data")
+
+// ....................... 
+
+// event.once("data" ,()=>{
+//     console.log("this is data event"); 
+// })
+// event.emit("data")
+// event.emit("data")
+
+
+// ....................... 
+
+// const handlerfn = () => {
+//     console.log("this is data event");
+// }
+// event.on("data", handlerfn)
+
+// event.emit("data")
+// event.emit("data")
+// event.emit("data")
+// event.off("data" ,handlerfn)
+// event.emit("data")
+
+
+const path = require("path")
+
+// let dirName = path.dirname("D:/mern630/module-mern")
+// console.log(dirName);
+
+// let baseName = path.basename("D:/mern630/module-mern")
+// console.log(baseName);
+
+// let newPath = path.join("folder" , "images" ,"page")
+// console.log(newPath);
+
+// let newResolvedPath = path.resolve("test.txt")
+// console.log(newResolvedPath);
+
+
+const http = require("http");
+
+// api - application programming interface
+// http - Hypertext Transfer Protocol
+
+const server = http.createServer((req,res)=>{
+    console.log("hello vishal");
+    res.end("hello grras")
+});
+server.listen(5000 ,()=>{
+    console.log("server is running on port 5000");
+});
+// understud
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
